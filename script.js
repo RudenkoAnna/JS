cec3c8d100bcd27cebf02f0302ebd94a8353edb8
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-02-23 11:58:17 +0200

    checked basic operations

---
+++ script.js
@@ -0,0 +1,9 @@
+let numberOne;
+let numberTwo;
+
+numberOne = prompt("Enter first number");
+numberTwo = prompt("Ënter your second number");
+alert("Multiplication " + "" + Number(numberOne) * Number(numberTwo));
+alert("Sum " + "" + (Number(numberOne) + Number(numberTwo)));
+alert("Division " + "" + Number(numberOne) / Number(numberTwo));
+alert("Substraction " + "" + (Number(numberOne) - Number(numberTwo)));
