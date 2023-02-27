3e09eeea83536dffeca50fc9d14bbec122bf9938
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-02-27 10:47:00 +0200

    performed 1st task
    when one of the number is = 0, we are alerring an error message

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let numberTwo;
 
 numberOne = prompt("Enter first number");
 numberTwo = prompt("Ënter your second number");
+if (numberOne == 0 || numberTwo == 0) {
+  alert(
+    "Looks like one of your number is equal to 0, it's impossible to proceed with division"
+  );
+} else {
+  alert("Division " + "" + Number(numberOne) / Number(numberTwo));
+}
 alert("Multiplication " + "" + Number(numberOne) * Number(numberTwo));
 alert("Sum " + "" + (Number(numberOne) + Number(numberTwo)));
-alert("Division " + "" + Number(numberOne) / Number(numberTwo));
+
 alert("Substraction " + "" + (Number(numberOne) - Number(numberTwo)));
