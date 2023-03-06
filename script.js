1b8aa9bcc69545dee1c4632fcef6de60effd4684
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-03-06 10:35:49 +0200

    added the semi tree

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const arr = [20, 30, 40, 90, 120, true, false, 11, 189, 1, 18, 31, "word"];
 const numbers = [];
+console.log(arr);
 
+//Displaying of sum (taking into account numbers only)
 let sum = 0;
-
 for (let i = 0; i < arr.length; i++) {
   if (typeof arr[i] === "number") {
     sum = sum + arr[i];
   }
 }
-console.log(arr);
-
 console.log(sum);
 
+//Displaying of max values (taking into account numbers only)
 let max = arr[0];
 for (let i = 1; i < 10; ++i) {
   if (typeof arr[i] === "number") {
@@ -21,8 +21,9 @@ for (let i = 1; i < 10; ++i) {
   }
 }
 console.log(max);
-let min = arr[0];
 
+//Displaying of min values (taking into account numbers only)
+let min = arr[0];
 for (let i = 1; i < 10; ++i) {
   if (typeof arr[i] === "number") {
     if (arr[i] < min) {
@@ -31,3 +32,13 @@ for (let i = 1; i < 10; ++i) {
   }
 }
 console.log(min);
+
+//Displaying of semi tree
+let depth = 5;
+let char = "#";
+let output = "";
+for (let i = 1; i <= depth; i++) {
+  output += char.repeat(i);
+  output += "\n";
+}
+console.log(output);
