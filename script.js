3612009362d6b8386b0b5efee24a370251e3f100
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-03-06 10:13:21 +0200

    added ffunction for checking min and max

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const arr = [20, 30, 40, 90, 120, 11, 189, 1, 18, 31];
+const arr = [20, 30, 40, 90, 120, 11, 189, 1, 18, 31, "word"];
 
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == isNaN(i)) {
+    continue;
+  }
   sum = sum + arr[i];
 }
+console.log(arr);
 
 console.log(sum);
+
+let max = arr[0];
+for (let i = 1; i < 10; ++i) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+console.log(max);
+let min = arr[0];
+for (let i = 1; i < 10; ++i) {
+  if (arr[i] < min) {
+    min = arr[i];
+  }
+}
+console.log(min);
