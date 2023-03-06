9c866405589a244f6717aeb3d800c5c043651166
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-03-06 10:30:21 +0200

    added check whether item is number

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const arr = [20, 30, 40, 90, 120, 11, 189, 1, 18, 31, "word"];
+const arr = [20, 30, 40, 90, 120, true, false, 11, 189, 1, 18, 31, "word"];
+const numbers = [];
 
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] == isNaN(i)) {
-    continue;
+  if (typeof arr[i] === "number") {
+    sum = sum + arr[i];
   }
-  sum = sum + arr[i];
 }
 console.log(arr);
 
@@ -14,15 +14,20 @@ console.log(sum);
 
 let max = arr[0];
 for (let i = 1; i < 10; ++i) {
-  if (arr[i] > max) {
-    max = arr[i];
+  if (typeof arr[i] === "number") {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
   }
 }
 console.log(max);
 let min = arr[0];
+
 for (let i = 1; i < 10; ++i) {
-  if (arr[i] < min) {
-    min = arr[i];
+  if (typeof arr[i] === "number") {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
   }
 }
 console.log(min);
