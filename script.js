0f2479f3f54bb7c229adf66bb30f1d82ae487f6b
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-03-10 15:23:01 +0200

    completed 1st task

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//function to check whether object is empty
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+const emptyObj = {};
+const nonEmptyObj = { name: "Anna", age: 30 };
+
+console.log(isEmpty(emptyObj));
+console.log(isEmpty(nonEmptyObj));
+
 // creating user object  +  and sayHello reply
 function createUser(name, age) {
   return {
@@ -23,3 +33,25 @@ const Brian = createUser("Brian", 29);
 
 showMessage(Anna);
 showMessage(Brian);
+
+/// calculator with sum and multiplication of 2 numbers
+const calculator = {
+  firstNumber: 0,
+  secondNumber: 0,
+
+  ask: function () {
+    this.firstNumber = Number(prompt("Введіть перше число:"));
+    this.secondNumber = Number(prompt("Введіть друге число:"));
+  },
+
+  sum: function () {
+    return this.firstNumber + this.secondNumber;
+  },
+
+  mul: function () {
+    return this.firstNumber * this.secondNumber;
+  },
+};
+calculator.ask();
+console.log(calculator.sum());
+console.log(calculator.mul());
