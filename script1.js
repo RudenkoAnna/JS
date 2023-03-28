aaa5231509e6ab619f6f7a10e23c9b46ee9a27ee
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-03-28 19:01:15 +0300

    completed 1st task

--- script1.js
+++ script1.js
@@ -1,6 +1,23 @@
-function Accumulator() {
-  this.accum = [];
+// created function and added increment + decrement options
+function Accumulator(insertedDigit) {
+  this.value = insertedDigit;
+  this.increment = function () {
+    this.value++;
+  };
+  this.decrement = function () {
+    this.value--;
+  };
 }
-TodoList.prototype.addItem = function (item) {
-  this.accum.push(item);
-};
+
+let insertedDigit = parseInt(prompt("Please insert the digit:"));
+let accumulator = new Accumulator(insertedDigit);
+
+console.log("Inserted value: " + accumulator.value);
+
+accumulator.increment();
+console.log("Increment: " + accumulator.value);
+
+accumulator.decrement();
+console.log("Decrement: " + accumulator.value);
+
+//created CancelableAccumulator function
