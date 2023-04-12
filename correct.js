edee97f3bf63a6774016d00c96cc36d1ccca5b25
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-04-12 19:46:25 +0300

    fixed 2

--- correct.js
+++ correct.js
@@ -16,3 +16,18 @@ let numbers = [1, 2, 3, 4, 5];
 
 let avg = calculateAverage(numbers);
 console.log("The average is: " + avg); // має бути 3
+// 2
+debugger;
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+User.prototype.sayHello = function () {
+  //added ${this. }
+  return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
+};
+
+const user = new User("Sherlock", 27);
+// Added () at the sayHello
+console.log(user.sayHello()); // має вивести "Hi. My name is Sherlock and I'm 27 years old"
--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// 2
-function User(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-User.prototype.sayHello = function () {
-  return `Hi. My name is {name} and I'm {this.age} years old`;
-};
-
-const user = new User("Sherlock", 27);
-console.log(user.sayHello); // має вивести "Hi. My name is Sherlock and I'm 27 years old"
-
 // 3
 const c = calculator();
 console.log(c()); // має вивести 1
