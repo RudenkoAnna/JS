be091b48394d2cac6afb25e51fb69298fd388a75
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-13 15:16:35 +0300

    created tests for less 23 +26

--- src/index.js
+++ src/index.js
@@ -51,4 +51,62 @@ for (let i = 1; i <= depth; i++) {
 }
 console.log(output);
 /// functions from lesson 26
+//function to check whether object is empty
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+const emptyObj = {};
+const nonEmptyObj = { name: "Anna", age: 30 };
+
+console.log(isEmpty(emptyObj));
+console.log(isEmpty(nonEmptyObj));
+
+// creating user object  +  and sayHello reply
+function createUser(name, age) {
+  return {
+    name: name,
+    age: age,
+    getNameAndAge() {
+      return `Привіт, я ${this.name}, мені ${this.age} років`;
+    },
+  };
+}
+
+const showMessage = function (user) {
+  if (!user) {
+    return;
+  }
+
+  const userInfo = user.getNameAndAge();
+  console.log(`${userInfo}`);
+};
+
+const Anna = createUser("Anna", 30);
+const Brian = createUser("Brian", 29);
+
+showMessage(Anna);
+showMessage(Brian);
+/// calculator with sum and multiplication of 2 numbers
+const calculator = {
+  firstNumber: 0,
+  secondNumber: 0,
+
+  ask: function () {
+    this.firstNumber = Number(prompt("Введіть перше число:"));
+    this.secondNumber = Number(prompt("Введіть друге число:"));
+  },
+
+  sum: function () {
+    return this.firstNumber + this.secondNumber;
+  },
+
+  mul: function () {
+    return this.firstNumber * this.secondNumber;
+  },
+};
+calculator.ask();
+console.log(calculator.sum());
+console.log(calculator.mul());
+44;
+
 //functions from lesson 28
--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,66 @@
-describe("Check user event", () => {
-  const desc = "<p>Check if button was clicked</p>";
+// tests for  lesson 23
+describe("Array Operations", () => {
+  let arr;
 
-  describe(desc, () => {
-    it("Check if button was clicked", () => {
-      spyOn(console, "log");
-      onClick({
-        target: "button",
-      });
-      expect(console.log).toHaveBeenCalled();
+  beforeEach(() => {
+    arr = [20, 30, 40, 90, 120, true, false, 11, 189, 1, 18, 31, "word"];
+  });
+
+  describe("Sum of Numbers", () => {
+    it("should calculate the sum of numbers in the array", () => {
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+          sum = sum + arr[i];
+        }
+      }
+      expect(sum).toBe(570);
+    });
+  });
+
+  describe("Maximum Value", () => {
+    it("should find the maximum value among the numbers in the array", () => {
+      let max = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+          if (arr[i] > max) {
+            max = arr[i];
+          }
+        }
+      }
+      expect(max).toBe(189);
+    });
+  });
+
+  describe("Minimum Value", () => {
+    it("should find the minimum value among the numbers in the array", () => {
+      let min = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+          if (arr[i] < min) {
+            min = arr[i];
+          }
+        }
+      }
+      expect(min).toBe(1);
     });
   });
 });
+describe("Semi Tree", () => {
+  describe("Generation", () => {
+    it("should generate the semi tree with the given depth", () => {
+      let depth = 5;
+      let char = "#";
+      let output = "";
+      for (let i = 1; i <= depth; i++) {
+        output += char.repeat(i);
+        output += "\n";
+      }
+      expect(output).toBe("#\n##\n###\n####\n#####\n");
+    });
+  });
+});
+
 //tests for lesson 26
 describe("isEmpty", () => {
   it("should return true for an empty object", () => {
@@ -56,6 +106,7 @@ describe("showMessage", () => {
 describe("calculator", () => {
   beforeEach(() => {
     spyOn(window, "prompt").and.returnValues("5", "3");
+    5;
   });
 
   it("should ask for the first and second numbers", () => {
