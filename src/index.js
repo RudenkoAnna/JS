8b99ca280d87432e7687aa1feca8714c5b3caef2
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-13 14:49:53 +0300

    added tests for lesson 23

--- src/index.js
+++ src/index.js
@@ -5,3 +5,50 @@ function onClick(e) {
 }
 
 btn.addEventListener("click", onClick);
+///functions from lesson 23
+const arr = [20, 30, 40, 90, 120, true, false, 11, 189, 1, 18, 31, "word"];
+const numbers = [];
+console.log(arr);
+
+//Displaying of sum (taking into account numbers only)
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === "number") {
+    sum = sum + arr[i];
+  }
+}
+console.log(sum);
+
+//Displaying of max values (taking into account numbers only)
+let max = arr[0];
+for (let i = 1; i < 10; ++i) {
+  if (typeof arr[i] === "number") {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+}
+console.log(max);
+
+//Displaying of min values (taking into account numbers only)
+let min = arr[0];
+for (let i = 1; i < 10; ++i) {
+  if (typeof arr[i] === "number") {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+}
+console.log(min);
+
+//Displaying of semi tree
+let depth = 5;
+let char = "#";
+let output = "";
+for (let i = 1; i <= depth; i++) {
+  output += char.repeat(i);
+  output += "\n";
+}
+console.log(output);
+/// functions from lesson 26
+//functions from lesson 28
--- tests/index.test.js
+++ tests/index.test.js
@@ -11,3 +11,59 @@ describe("Check user event", () => {
     });
   });
 });
+// tests for  lesson 23
+
+describe("Array Operations", () => {
+  let arr;
+
+  beforeEach(() => {
+    arr = [20, 30, 40, 90, 120, true, false, 11, 189, 1, 18, 31, "word"];
+  });
+
+  it("should calculate the sum of numbers in the array", () => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === "number") {
+        sum = sum + arr[i];
+      }
+    }
+    expect(sum).toBe(440);
+  });
+
+  it("should find the maximum value among the numbers in the array", () => {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (typeof arr[i] === "number") {
+        if (arr[i] > max) {
+          max = arr[i];
+        }
+      }
+    }
+    expect(max).toBe(189);
+  });
+
+  it("should find the minimum value among the numbers in the array", () => {
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (typeof arr[i] === "number") {
+        if (arr[i] < min) {
+          min = arr[i];
+        }
+      }
+    }
+    expect(min).toBe(1);
+  });
+});
+
+describe("Semi Tree", () => {
+  it("should generate the semi tree with the given depth", () => {
+    let depth = 5;
+    let char = "#";
+    let output = "";
+    for (let i = 1; i <= depth; i++) {
+      output += char.repeat(i);
+      output += "\n";
+    }
+    expect(output).toBe("#\n##\n###\n####\n#####\n");
+  });
+});
