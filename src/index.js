7fc9992d16668e6e67f73d9672621ecc80e888a0
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-06-13 15:28:33 +0300

    added all tests
    at the folder test you can see file test.js ( so I copied all the content to this file ( since jasmine is showing just a 3 sections of the tests, I checked the code from lessons separatelly )

--- src/index.js
+++ src/index.js
@@ -110,3 +110,29 @@ console.log(calculator.mul());
 44;
 
 //functions from lesson 28
+document.addEventListener("keydown", (e) => {
+  if (e.key.toLowerCase() === "f") {
+    e.preventDefault();
+    scrollToElement("f");
+  } else if (e.key.toLowerCase() === "a") {
+    e.preventDefault();
+    scrollToElement("a");
+  } else if (e.key.toLowerCase() === "b") {
+    e.preventDefault();
+    scrollToElement("b");
+  } else if (e.key.toLowerCase() === "c") {
+    e.preventDefault();
+    scrollToElement("c");
+  } else if (e.key.toLowerCase() === "d") {
+    e.preventDefault();
+    scrollToElement("d");
+  } else if (e.key.toLowerCase() === "e") {
+    e.preventDefault();
+    scrollToElement("e");
+  }
+});
+
+function scrollToElement(elementId) {
+  const element = document.getElementById(elementId);
+  element.scrollIntoView({ behavior: "smooth" });
+}
--- tests/index.test.js
+++ tests/index.test.js
@@ -11,6 +11,69 @@ describe("Check user event", () => {
     });
   });
 });
+// tests for  lesson 23
+describe("Array Operations", () => {
+  let arr;
+
+  beforeEach(() => {
+    arr = [20, 30, 40, 90, 120, true, false, 11, 189, 1, 18, 31, "word"];
+  });
+
+  describe("Sum of Numbers", () => {
+    it("should calculate the sum of numbers in the array", () => {
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+          sum = sum + arr[i];
+        }
+      }
+      expect(sum).toBe(570);
+    });
+  });
+
+  describe("Maximum Value", () => {
+    it("should find the maximum value among the numbers in the array", () => {
+      let max = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+          if (arr[i] > max) {
+            max = arr[i];
+          }
+        }
+      }
+      expect(max).toBe(189);
+    });
+  });
+
+  describe("Minimum Value", () => {
+    it("should find the minimum value among the numbers in the array", () => {
+      let min = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+          if (arr[i] < min) {
+            min = arr[i];
+          }
+        }
+      }
+      expect(min).toBe(1);
+    });
+  });
+});
+describe("Semi Tree", () => {
+  describe("Generation", () => {
+    it("should generate the semi tree with the given depth", () => {
+      let depth = 5;
+      let char = "#";
+      let output = "";
+      for (let i = 1; i <= depth; i++) {
+        output += char.repeat(i);
+        output += "\n";
+      }
+      expect(output).toBe("#\n##\n###\n####\n#####\n");
+    });
+  });
+});
+
 //tests for lesson 26
 describe("isEmpty", () => {
   it("should return true for an empty object", () => {
@@ -56,6 +119,7 @@ describe("showMessage", () => {
 describe("calculator", () => {
   beforeEach(() => {
     spyOn(window, "prompt").and.returnValues("5", "3");
+    5;
   });
 
   it("should ask for the first and second numbers", () => {
@@ -71,5 +135,87 @@ describe("calculator", () => {
   it("should return the multiplication of the first and second numbers", () => {
     const multiplication = calculator.mul();
     expect(multiplication).toBe(15);
+  });
+});
+
+///tests for lesson 28
+describe("scrollToElement", () => {
+  let element;
+
+  beforeEach(() => {
+    element = document.createElement("div");
+    element.id = "testElement";
+    document.body.appendChild(element);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(element);
+  });
+
+  it("should scroll to the element with the given ID", () => {
+    spyOn(element, "scrollIntoView");
+    scrollToElement("testElement");
+    expect(element.scrollIntoView).toHaveBeenCalledWith({ behavior: "smooth" });
+  });
+
+  it("should not scroll if the element ID is not found", () => {
+    spyOn(element, "scrollIntoView");
+    scrollToElement("nonExistentElement");
+    expect(element.scrollIntoView).not.toHaveBeenCalled();
+  });
+});
+
+describe("keydown event listener", () => {
+  beforeEach(() => {
+    spyOn(window, "scrollToElement");
+  });
+
+  it("should scroll to element 'f' when 'f' key is pressed", () => {
+    const event = new KeyboardEvent("keydown", { key: "F" });
+    document.dispatchEvent(event);
+    expect(window.scrollToElement).toHaveBeenCalledWith("f");
+    expect(event.defaultPrevented).toBe(true);
+  });
+
+  it("should scroll to element 'a' when 'a' key is pressed", () => {
+    const event = new KeyboardEvent("keydown", { key: "A" });
+    document.dispatchEvent(event);
+    expect(window.scrollToElement).toHaveBeenCalledWith("a");
+    expect(event.defaultPrevented).toBe(true);
+  });
+
+  it("should scroll to element 'b' when 'b' key is pressed", () => {
+    const event = new KeyboardEvent("keydown", { key: "B" });
+    document.dispatchEvent(event);
+    expect(window.scrollToElement).toHaveBeenCalledWith("b");
+    expect(event.defaultPrevented).toBe(true);
+  });
+
+  it("should scroll to element 'c' when 'c' key is pressed", () => {
+    const event = new KeyboardEvent("keydown", { key: "C" });
+    document.dispatchEvent(event);
+    expect(window.scrollToElement).toHaveBeenCalledWith("c");
+    expect(event.defaultPrevented).toBe(true);
+  });
+
+  it("should scroll to element 'd' when 'd' key is pressed", () => {
+    const event = new KeyboardEvent("keydown", { key: "D" });
+    document.dispatchEvent(event);
+    expect(window.scrollToElement).toHaveBeenCalledWith("d");
+    expect(event.defaultPrevented).toBe(true);
+  });
+
+  it("should scroll to element 'e' when 'e' key is pressed", () => {
+    const event = new KeyboardEvent("keydown", { key: "E" });
+    document.dispatchEvent(event);
+    expect(window.scrollToElement).toHaveBeenCalledWith("e");
+    expect(event.defaultPrevented).toBe(true);
+  });
+
+  it("should not scroll when a different key is pressed", () => {
+    const event = new KeyboardEvent("keydown", { key: "X" });
+    document.dispatchEvent(event);
+    expect(window.scrollToElement).not.toHaveBeenCalled();
+    expect(event.defaultPrevented).toBe(false);
   });
 });
